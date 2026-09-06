39d84eb3fde4a5a58eac0776f90ca56b45b9b1f7
Nikolai Hovis <nikolai.hovis@gmail.com>
2026-09-06 16:34:04 -0500

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using Alta;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(ViridiumBuff.Core), "ViridiumBuff", "1.0.0", "CGNik", null)]
+[assembly: MelonGame("Alta", "A Township Tale")]
+
+namespace ViridiumBuff
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            PhysicalMaterial physicalMaterial_viridium = PhysicalMaterial.All.Where(mat => mat.Hash == 16222u).First();
+            typeof(PhysicalMaterial).GetField("damageMultiplier", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(physicalMaterial_viridium, 2f);
+            typeof(PhysicalMaterial).GetField("durabilityMultiplier", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(physicalMaterial_viridium, 4.5f);
+        }
+    }
+}
